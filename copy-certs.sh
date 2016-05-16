a8ca8a22b9fcd4d09f8d341ae112bc13e0b77d17
Marko Vuksanovic <markovuksanovic@gmail.com>
2016-05-16 20:48:42 +1000

    Run script from parent directory

--- copy-certs.sh
+++ copy-certs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 while read line; do
-  if [ -f letsencrypt.sh/certs/${line}/privkey.pem ]; then
-    cp letsencrypt.sh/certs/${line}/privkey.pem /etc/nginx/certs/${line}.key
+  if [ -f ../letsencrypt.sh/certs/${line}/privkey.pem ]; then
+    cp ../letsencrypt.sh/certs/${line}/privkey.pem /etc/nginx/certs/${line}.key
     echo "Copied private key for ${line}."
   else
-    echo "Certificate for ${line} not found at letsencrypt.sh/certs/${line}/privkey.pem. Skipping..."
+    echo "Certificate for ${line} not found at ../letsencrypt.sh/certs/${line}/privkey.pem. Skipping..."
   fi
-  if [ -f letsencrypt.sh/certs/${line}/fullchain.pem ]; then
-    cp letsencrypt.sh/certs/${line}/fullchain.pem /etc/nginx/certs/${line}.crt
+  if [ -f ../letsencrypt.sh/certs/${line}/fullchain.pem ]; then
+    cp ../letsencrypt.sh/certs/${line}/fullchain.pem /etc/nginx/certs/${line}.crt
     echo "Copied public key for ${line}."
   else
-    echo "Certificate for ${line} not found at letsencrypt.sh/certs/${line}/fullchain. Skipping..."
+    echo "Certificate for ${line} not found at ../letsencrypt.sh/certs/${line}/fullchain. Skipping..."
   fi
 done < "$@"
